90fe09072bb2a9dee36797cf35a0f9158257470b
fitDizzle <markfitnessaz@gmail.com>
2022-05-20 00:07:08 -0700

    sending to heroku

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'bf0de1a9d27a9f'
+    password = '7b4a3c3c276b9c'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments {comments}</li></ul>"
+
+    sender_email = 'from@example.com'
+    receiver_email = 'to@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
